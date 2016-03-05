e7a4cb323b523917a7a3052ac59d33983cb06216
Anderson Marin <dam.marinramos@gmail.com>
2016-03-05 16:28:25 -0300

    More EventListener

--- projeto/calcula-imc.js
+++ projeto/calcula-imc.js
@@ -1,38 +1,4 @@
-function calculaTodosImcs(){
-	//Pegando os valores da tr de classe paciente e jogando em um array de nome trsPacientes
-	var trsPacientes = document.getElementsByClassName("paciente"); 
 
-	percorreArray(trsPacientes, function(pacienteTr){
-
-		// pegando os valores pela class do tr e salvando nas variaveis usando o pacienteTr pra saber a posicao
-		var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
-		var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
-		var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
-
-
-		//criando objeto
-		var pacienteAtual = {"nome" : tdNome.textContent ,
-							 "peso" : tdPeso.textContent ,
-							 "altura" : tdAltura.textContent,
-							 "pegaImc" : function(){//criando função anonima
-							 	if (this.altura != 0){
-									var imc = this.peso / (this.altura * this.altura);
-									return imc;
-								} 
-								else{
-									console.log("Nao executei porque a altura eh igual a zero");
-								}
-							 }};
-
-		var imc = pacienteAtual.pegaImc();
-
-		//colocando o valor na variavel pra exibir no campo imc
-		var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
-			tdImc.textContent = imc;
-
-			console.log(imc);
-	});
-}
 
 //adicionando controle de eventos, pra só calcular o imc quando o usuario clicar no botão em HTML
 //porem, usando o atributo onclick estou preso a somente uma ação, vou usar o addEventListener pra contornar isso
@@ -40,4 +6,38 @@ function calculaTodosImcs(){
 //botao.onclick = calculaTodosImcs;
 
 var botao = document.getElementById("calcula-imcs");
-botao.addEventListener("click", calculaTodosImcs);
+botao.addEventListener("click", function(){
+								//Pegando os valores da tr de classe paciente e jogando em um array de nome trsPacientes
+								var trsPacientes = document.getElementsByClassName("paciente"); 
+
+								percorreArray(trsPacientes, function(pacienteTr){
+
+									// pegando os valores pela class do tr e salvando nas variaveis usando o pacienteTr pra saber a posicao
+									var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
+									var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
+									var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
+
+
+									//criando objeto
+									var pacienteAtual = {"nome" : tdNome.textContent ,
+														 "peso" : tdPeso.textContent ,
+														 "altura" : tdAltura.textContent,
+														 "pegaImc" : function(){//criando função anonima
+														 	if (this.altura != 0){
+																var imc = this.peso / (this.altura * this.altura);
+																return imc;
+															} 
+															else{
+																console.log("Nao executei porque a altura eh igual a zero");
+															}
+														 }};
+
+									var imc = pacienteAtual.pegaImc();
+
+									//colocando o valor na variavel pra exibir no campo imc
+									var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
+										tdImc.textContent = imc;
+
+										console.log(imc);
+								});
+							});
