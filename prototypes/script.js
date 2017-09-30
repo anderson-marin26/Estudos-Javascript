b20159e2ca81eede2224ceedc900939b67c3ec19
Anderson Marin de Ramos <dam.marinramos@gmail.com>
2017-09-30 17:10:38 -0300

    Using constructor, ES6 Classes

--- prototypes/script.js
+++ prototypes/script.js
@@ -1,1 +1,20 @@
-display('Hello World')
+'use strict';
+
+class Cat
+{
+	constructor(name, color)
+	{
+		this.name = name
+		this.color = color
+	}
+
+	speak()
+	{
+		display('Meow')
+	}
+}
+
+var cat = new Cat('Minerva', 'White')
+
+display(cat)
+cat.speak()
