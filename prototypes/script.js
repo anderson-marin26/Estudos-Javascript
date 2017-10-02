2b412e0db42ea4efb3efbbb60467cb8e8bdc159b
Anderson Marin de Ramos <dam.marinramos@gmail.com>
2017-10-01 21:36:21 -0300

    Using objects setters and getters

--- prototypes/script.js
+++ prototypes/script.js
@@ -15,7 +15,10 @@ class Cat
 }
 
 var dog = {
-	name: 'dogo',
+	name: {
+		first: 'dogo',
+		last: 'bamboozled'
+	},
 	color: 'black'
 }
 
@@ -31,3 +34,28 @@ for (var propertyName in dog)
 {
 	display(propertyName + ': ' + dog[propertyName])
 }
+
+/*
+** Setters and Getters
+*/
+Object.defineProperty(dog, 'fullName',
+{
+	get: function()	
+	{
+		return this.name.first + ' ' + this.name.last
+	},
+
+	set: function(value)
+	{
+		var nameParts = value.split(' ')
+		this.name.first = nameParts[0]
+		this.name.last = nameParts[1]
+	}
+})
+
+display('Using getter to show name: ')
+display(dog.fullName)
+
+dog.fullName = 'DOGO SUPREME'
+display('Set dogo name using setter')
+display(dog.fullName)
